ece1c836d2fa74511b4c08408e3203e25958aef7
DEV07\gabriel.a <gabriel.a@starkey.co.il>
2022-05-16 18:12:25 +0300

    checkbox

--- main.py
+++ main.py
@@ -1091,40 +1091,43 @@ from tkinter import *
 #
 #
 #################### Entrybox ####################
+# from tkinter import *
+# # entry widget = textbox that accepts a single line of user input
+#
+# def submit():
+#     username = entry.get()
+#     print(f"Hello {username}")
+#     entry.config(state=DISABLED)
+# def delete():
+#     entry.delete(0,END)
+#
+# def backspace():
+#     entry.delete(len(entry.get())-1 ,END)
+# window = Tk()
+#
+# entry = Entry(window,
+#               font=("Ariel",15),
+#               fg="#00FF00",
+#               bg="black",
+#               show="*")
+# # entry.insert(0,"Spongebob")
+# entry.pack(side=LEFT)
+#
+# submit_button = Button(window,text="Submit",command=submit)
+# submit_button.pack(side=RIGHT)
+#
+# delete_button = Button(window,text="Delete",command=delete)
+# delete_button.pack(side=RIGHT)
+#
+# backspace_button = Button(window,text="backspace",command=backspace)
+# backspace_button.pack(side=RIGHT)
+#
+#
+# window.mainloop()
+
+
+#################### checkbox ####################
 from tkinter import *
-# entry widget = textbox that accepts a single line of user input
-
-def submit():
-    username = entry.get()
-    print(f"Hello {username}")
-    entry.config(state=DISABLED)
-def delete():
-    entry.delete(0,END)
-
-def backspace():
-    entry.delete(len(entry.get())-1 ,END)
-window = Tk()
-
-entry = Entry(window,
-              font=("Ariel",15),
-              fg="#00FF00",
-              bg="black",
-              show="*")
-# entry.insert(0,"Spongebob")
-entry.pack(side=LEFT)
-
-submit_button = Button(window,text="Submit",command=submit)
-submit_button.pack(side=RIGHT)
-
-delete_button = Button(window,text="Delete",command=delete)
-delete_button.pack(side=RIGHT)
-
-backspace_button = Button(window,text="backspace",command=backspace)
-backspace_button.pack(side=RIGHT)
-
-
-window.mainloop()
-
 
 
 
